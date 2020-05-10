94774434cb1ab08ea1d5d2e9e5b71d64299d7331
Octoxalis <octoxalis@gmail.com>
2020-05-10 09:47:32 +0200

    [site] webmanifest start url

--- source/matrix/assets/scripts/js/parts/_service_.js
+++ source/matrix/assets/scripts/js/parts/_service_.js
@@ -12,7 +12,7 @@ SERVICE_o.init__v = url_s =>
   //-- const SUCCESS_s = 'successful'
   const FAILURE_s = 'failed'
   
-  navigator.serviceWorker.register( url_s, {scope: '{{U_o.url_s}}'} )
+  navigator.serviceWorker.register( url_s, {scope: '../../../'} )
   .then( null,    //-- .then( registration => console.log(  `${REGISTRATION_s} ${SUCCESS_s} [scope: ${registration.scope}]` ),
          error_o => console.log( `${REGISTRATION_s} ${FAILURE_s} [error: ${error_o}]` ) )
 }
